84b48a250537b98412067b1da2ac20b5536e2a79
Guillermo Martinez Espina <gmotzespina@icloud.com>
2021-12-11 11:33:09 -0600

    Pushes lecture-pro code

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# CourseProject
+# Lecture Pro
 
-Please fork this repository and paste the github link of your fork on Microsoft CMT. Detailed instructions are on Coursera under Week 1: Course Project Overview/Week 9 Activities.
+## Presentation
+
+### Video
+
+### Slides
+
+https://docs.google.com/presentation/d/1Db4IFuDABlomjgAOj_SmtoOUdC_8TYiK0fARS8e_Yu8/edit?usp=sharing
+
+## How to run
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `yarn build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+---
+
+# Useful Links
+
+- PDF Express - https://pdfjs.express/profile
+
+- PDF tron CORS - https://community.pdftron.com/t/error-loading-pdf-viewer-different-domain/4536
---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import "./App.css";
+import WebViewer from "@pdftron/pdfjs-express-viewer";
+import { useEffect, useRef, useState } from "react";
+
+interface CoreInstance {
+  clearSearchResults: () => null;
+  UI: {
+    searchTextFull: (pattern: String, options?: any) => null;
+  };
+}
+
+function App() {
+  const viewer = useRef(null);
+  const [coreInstance, setCoreInstance] = useState<CoreInstance>();
+  const [UIInstance, setUIInstance] = useState<any>();
+
+  // const docViewer = new DocumentViewer();
+
+  useEffect(() => {
+    WebViewer(
+      {
+        path: "webviewer/lib",
+        initialDoc: "assets/book.pdf",
+        licenseKey: "", //Add licence key here
+      },
+      viewer.current
+    ).then((instance) => {
+      // now you can access APIs through the WebViewer instance
+      const { Core, UI } = instance;
+
+      setCoreInstance(Core.documentViewer);
+      setUIInstance(UI);
+
+      Core.documentViewer.zoomTo(1, 0, 0);
+      // adding an event listener for when a document is loaded
+      Core.documentViewer.addEventListener("documentLoaded", () => {
+        console.log("document loaded");
+      });
+    });
+  }, []);
+
+  const onClearSearchResults = () => {
+    if (coreInstance) {
+      coreInstance.clearSearchResults();
+    }
+  };
+
+  const onSearch = () => {
+    if (coreInstance) {
+      console.log(
+        UIInstance.searchTextFull("Text Information Systems", {
+          caseSensitive: true,
+        })
+      );
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="reading-title">
+        <h1>Text Information Systems</h1>
+      </div>
+      <div className="sidebar">
+        <header className="App-header">
+          <p className="extension-header">This is my chrome extension</p>
+          <button onClick={onClearSearchResults}>clearSearchResults</button>
+          <button onClick={onSearch}>perform search</button>
+        </header>
+        <div className="webviewer" ref={viewer}></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
